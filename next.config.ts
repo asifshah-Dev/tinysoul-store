628e97364ddcf39cb9d5b0711bbf29b1f051b99a
Asif Shah <asifshah9530608@gmail.com>
2026-09-10 21:09:13 +0500

    Add static export and unoptimized images for Hostinger

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -30,4 +32,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
